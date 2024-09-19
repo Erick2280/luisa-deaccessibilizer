f6c59de063a3d19c1d9a88fb367818cb1ba15258
Erick Almeida <Erick2280@users.noreply.github.com>
2024-09-18 21:00:57 -0300

    test: include test for using `substituteWithComment`option

--- tests/rules/accessibility-element-modifier-remover.spec.ts
+++ tests/rules/accessibility-element-modifier-remover.spec.ts
@@ -20,4 +20,13 @@ describe('AccessibilityElementModifierRemover', () => {
       [rule],
     );
   });
+
+  it('runs the rule successfully for Enzo example when `substitute with comment` is enabled', async () => {
+    await expectIsSameFileAfterApplyingRules(
+      `${SWIFT_FILE_SAMPLES_BASE_PATH}/Elane.swift`,
+      `${SWIFT_FILE_SAMPLES_BASE_PATH}/Elane_noAccessibilityElementModifierWithComments.swift`,
+      [rule],
+      { substituteWithComment: 'Code removed here' },
+    );
+  });
 });
